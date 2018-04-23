8f7f3184203952bbdd62511dfeb3260b640ed831
XSize <753355530@qq.com>
2018-04-24 00:00:55 +0800

    '添加了navigation和Drawer的使用'

--- app/src/main/java/com/jinlong/newmaterialdesign/MainActivity.java
+++ app/src/main/java/com/jinlong/newmaterialdesign/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.jinlong.newmaterialdesign.navigation.NavigationActivity;
 import com.jinlong.newmaterialdesign.toolbar.DefaultToolbarActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,5 +24,13 @@ public class MainActivity extends AppCompatActivity {
     public void toolbar(View view) {
         Intent toolbarIntent = new Intent(this, DefaultToolbarActivity.class);
         startActivity(toolbarIntent);
+    }
+
+    /**
+     * NavigationAndDrawer的联合使用
+     */
+    public void NavigationAndDrawer(View view) {
+        Intent navationIntent = new Intent(this, NavigationActivity.class);
+        startActivity(navationIntent);
     }
 }
---
+++ app/src/main/java/com/jinlong/newmaterialdesign/navigation/NavigationActivity.java
@@ -0,0 +1,15 @@
+package com.jinlong.newmaterialdesign.navigation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jinlong.newmaterialdesign.R;
+
+public class NavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+    }
+}
